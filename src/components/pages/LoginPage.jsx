1e3f45d4545bdc88ee86df763961fd26470630dc
nasredine kherfi <nasredine.kherfi@oclock.school>
2024-12-20 19:13:42 +0100

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  // State
+  const [inputValue, setInputValue] = useState("Bob");
+
+  //Comportement
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  //Affichage
+  return (
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prénom"
+          required
+        />
+        <button>Accédez à votre espace</button>
+      </form>
+    </div>
+  );
 }
