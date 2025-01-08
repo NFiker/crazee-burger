fa2bf183baf9a5fdcb47931b120ec3bafb0b1fd3
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-01-08 21:08:20 +0100

    ✨ F04: ORDER > Navbar > reload on click logo

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import Navbar from "./Navbar";
 import Main from "./Main";
-import { theme } from "../../../theme";
+import { theme } from "../../../theme/index";
 
 export default function OrderPage() {
   //State
@@ -22,14 +22,14 @@ export default function OrderPage() {
 }
 
 const OrderPageStyled = styled.div`
-  background: orange;
+  background: $;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: ${theme.colors.primary};
 
   .container {
-    background: red;
     height: 95vh;
     margin: 0 1rem;
     width: 1400px;
