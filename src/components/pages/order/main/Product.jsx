f43a6dce31700f2997c4f6edf74555383ba28670
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-01-10 20:40:45 +0100

    ✨ F05: ORDER > PRODUCT > import utils/maths : formatPrice

--- src/components/pages/order/main/Product.jsx
+++ src/components/pages/order/main/Product.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme/index";
 import PrimaryButton from "../../../reusable-ui/PrimaryButton";
+import { formatPrice } from "../../../../utils/maths";
 
 export default function Product({ title, price, imageSource }) {
   return (
@@ -11,7 +12,7 @@ export default function Product({ title, price, imageSource }) {
       <div className="text-info">
         <div className="title">{title}</div>
         <div className="description">
-          <div className="left-description">{price}</div>
+          <div className="left-description">{formatPrice(price)}</div>
           <div className="right-description">
             <PrimaryButton className="primary-button" label="Ajouter" />
           </div>
