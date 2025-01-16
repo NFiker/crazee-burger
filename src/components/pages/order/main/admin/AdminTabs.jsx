d43a9487e34e41c0728971130e4e1c39d09642b3
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-01-16 15:58:52 +0100

    ✨  F07: ADMIN > Tab > add label

--- src/components/pages/order/main/admin/AdminTabs.jsx
+++ src/components/pages/order/main/admin/AdminTabs.jsx
@@ -1,17 +1,25 @@
 import styled from "styled-components";
 import Tab from "../../../../reusable-ui/Tab";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
 import { theme } from "../../../../../theme/index";
 
 export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
+  const handleClick = () => {
+    setIsCollapsed(!isCollapsed);
+  };
   return (
     <AdminTabsStyled>
       <Tab
         Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
-        onClick={() => {
-          setIsCollapsed(!isCollapsed);
-        }}
+        onClick={handleClick}
         className={isCollapsed ? "is-active" : ""}
+      />
+      <Tab
+        Icon={<AiOutlinePlus />}
+        onClick={handleClick}
+        className={isCollapsed ? "is-active" : ""}
+        label={"Ajouter un produit"}
       />
     </AdminTabsStyled>
   );
@@ -25,5 +33,9 @@ const AdminTabsStyled = styled.div`
     background: ${theme.colors.background_dark};
     border-color: ${theme.colors.background_dark};
     color: ${theme.colors.white};
+  }
+
+  button {
+    margin-left: 1px;
   }
 `;
