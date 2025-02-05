17eb0f7375065a8055d75ee655f7d30554b12e92
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-02-05 12:48:37 +0100

    ✨F10: BASKET > Add BasketBody

--- src/components/pages/order/main/basket/Basket.jsx
+++ src/components/pages/order/main/basket/Basket.jsx
@@ -4,6 +4,7 @@ import Header from "../../../../reusable-ui/Header";
 import Total from "./Total";
 import { formatPrice } from "../../../../../utils/maths";
 import Footer from "./Footer";
+import BasketBody from "./BasketBody";
 
 export default function Basket() {
   return (
@@ -11,20 +12,13 @@ export default function Basket() {
       <Header>
         <Total amountToPay={formatPrice(0)} />
       </Header>
-      <div className="body">body</div>
+      <BasketBody />
       <Footer />
     </BasketStyled>
   );
 }
 
 const BasketStyled = styled.div`
-  background: pink;
   display: flex;
   flex-direction: column;
-
-  .body {
-    flex: 1;
-    background: ${theme.colors.background_white};
-    box-shadow: ${theme.shadows.basket};
-  }
 `;
