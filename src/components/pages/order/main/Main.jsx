dcba7d20ea6d801019829985f08a42218e9bd3d6
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-02-05 11:42:54 +0100

    ♻️ F10: BASKET > create reusable Header

--- src/components/pages/order/main/Main.jsx
+++ src/components/pages/order/main/Main.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme";
-import Basket from "./Basket";
+import Basket from "./basket/Basket";
 import Menu from "./mainRightSide/menu/Menu";
 import MainRightSide from "./mainRightSide/menu/MainRightSide";
 
--- src/components/pages/order/main/basket/Basket.jsx
+++ src/components/pages/order/main/basket/Basket.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
-import { theme } from "../../../../theme";
+import { theme } from "../../../../../theme";
+import Header from "../../../../reusable-ui/Header";
 
 export default function Basket() {
   return (
     <BasketStyled>
-      <div className="head">head</div>
+      <Header>Header</Header>
       <div className="body">body</div>
-      <div className="footer">footer</div>
+      <Header>Footer</Header>
     </BasketStyled>
   );
 }
@@ -16,17 +17,9 @@ const BasketStyled = styled.div`
   display: flex;
   flex-direction: column;
 
-  .head {
-    height: 70px;
-    background: ${theme.colors.background_dark};
-  }
   .body {
     flex: 1;
     background: ${theme.colors.background_white};
     box-shadow: ${theme.shadows.basket};
-  }
-  .footer {
-    height: 70px;
-    background: ${theme.colors.background_dark};
   }
 `;
