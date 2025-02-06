0776ea520dbc1ce914ebe21184853bdba0f379ce
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-02-06 10:45:38 +0100

    ♻️ F10: ORDERPAGE > extract menu CRUD into custom hooks useMenu

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -4,58 +4,18 @@ import { theme } from "../../../theme/index";
 import { useState, useRef } from "react";
 import Navbar from "./navbar/Navbar";
 import OrderContext from "../../../context/OrderContext";
-import { fakeMenu } from "../../../fakeData/fakeMenu";
 import { EMPTY_PRODUCT } from "../../../enums/product";
-import { deepClone } from "../../../utils/array";
+import { useMenu } from "../../../hooks/useMenu";
 
 export default function OrderPage() {
   //State
   const [isModeAdmin, setIsModeAdmin] = useState(true);
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [currentTabSelected, setCurrentTabSelected] = useState("edit");
-  const [menu, setMenu] = useState(fakeMenu.MEDIUM);
   const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
   const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
   const titleEditRef = useRef();
-
-  //Comportements
-  const handleAdd = (newProduct) => {
-    // 1. Copie du tableau
-    const menuCopy = [...menu];
-    // 2. Manip de la copie du tableau
-    const menuUpdated = [newProduct, ...menuCopy];
-    // 3. Update du state
-    setMenu(menuUpdated);
-  };
-
-  const handleDelete = (productIdDelete) => {
-    // 1. Copie du tableau
-    const menuCopy = deepClone(menu);
-    // 2. Manip de la copie du tableau
-    const menuUpdated = menuCopy.filter(
-      (product) => product.id !== productIdDelete
-    );
-    // 3. Update du state
-    setMenu(menuUpdated);
-  };
-
-  const handleEdit = (productBeingEdited) => {
-    // 1. Copie du tableau
-    const menuCopy = deepClone(menu);
-
-    // 2. Manip de la copie du state
-    const indexOfProductToEdit = menu.findIndex(
-      (menuProduct) => menuProduct.id === productBeingEdited.id
-    );
-    menuCopy[indexOfProductToEdit] = productBeingEdited;
-
-    // 3. Update du state
-    setMenu(menuCopy);
-  };
-
-  const resetMenu = () => {
-    setMenu(fakeMenu.SMALL);
-  };
+  const { menu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu();
 
   const orderContextValue = {
     isModeAdmin,
---
+++ src/hooks/useMenu.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { fakeMenu } from "../fakeData/fakeMenu";
+import { deepClone } from "../utils/array";
+
+export const useMenu = () => {
+  const [menu, setMenu] = useState(fakeMenu.MEDIUM);
+
+  //Comportements
+  const handleAdd = (newProduct) => {
+    // 1. Copie du tableau
+    const menuCopy = [...menu];
+    // 2. Manip de la copie du tableau
+    const menuUpdated = [newProduct, ...menuCopy];
+    // 3. Update du state
+    setMenu(menuUpdated);
+  };
+
+  const handleDelete = (productIdDelete) => {
+    // 1. Copie du tableau
+    const menuCopy = deepClone(menu);
+    // 2. Manip de la copie du tableau
+    const menuUpdated = menuCopy.filter(
+      (product) => product.id !== productIdDelete
+    );
+    // 3. Update du state
+    setMenu(menuUpdated);
+  };
+
+  const handleEdit = (productBeingEdited) => {
+    // 1. Copie du tableau
+    const menuCopy = deepClone(menu);
+
+    // 2. Manip de la copie du state
+    const indexOfProductToEdit = menu.findIndex(
+      (menuProduct) => menuProduct.id === productBeingEdited.id
+    );
+    menuCopy[indexOfProductToEdit] = productBeingEdited;
+
+    // 3. Update du state
+    setMenu(menuCopy);
+  };
+
+  const resetMenu = () => {
+    setMenu(fakeMenu.SMALL);
+  };
+
+  return { menu, handleAdd, handleDelete, handleEdit, resetMenu };
+};
