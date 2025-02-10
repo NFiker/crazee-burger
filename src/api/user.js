59c41c2bc990eecef222ba561b92c30fd089ebe1
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-02-10 17:58:59 +0100

    ✨ F13: API > add getter and setter

---
+++ src/api/user.js
@@ -0,0 +1,30 @@
+import { getDoc, setDoc, doc } from "firebase/firestore";
+import { db } from "./firebase-config";
+import { fakeMenu } from "../fakeData/fakeMenu";
+
+export const getUser = async (userId) => {
+  // const docref = doc(CHEMIN)
+  const docRef = doc(db, "users", userId);
+
+  const docSnapshot = await getDoc(docRef);
+  console.log("docSnapshot: ", docSnapshot);
+
+  if (docSnapshot.exists()) {
+    const userReceived = docSnapshot.data();
+    return userReceived;
+  }
+};
+
+export const createUser = (userId) => {
+  // CACHETTE
+  const docRef = doc(db, "users", userId);
+
+  // NOURRITURE
+  const newDoc = {
+    username: userId,
+    menu: fakeMenu.LARGE,
+  };
+
+  // setDoc(CACHETTE, NOURRITURE);
+  setDoc(docRef, newDoc);
+};
