38412993acc780512865fa5d0fb781456bd646bd
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-02-14 17:51:20 +0100

    🚧  F14: BASKET > Total > create CasinoEffect without changing UI

---
+++ src/components/reusable-ui/CasinoEffect.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function CasinoEffect({ count, className }) {
+  return <span className={className}>{count}</span>;
+}
+
+const CasinoEffectStyled = styled.div``;
